279a328d3c0cc88f22fadc92b8e3a6f45a94ea71
Jessica <jessica.bonomelli@beefree.io>
2022-10-13 19:38:22 +0200

    serializer

---
+++ app/user/urls.py
@@ -0,0 +1,14 @@
+"""
+URL mappings for the user API.
+"""
+from django.urls import path
+
+from user import views
+
+
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreteuserView.as_view(), name='create'),
+    path('token/', views.CreteTokenView.as_view,(), name='token'),
+]
