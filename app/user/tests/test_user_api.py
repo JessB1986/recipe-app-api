bea42e8434503971b02429fd612d9db2069132f3
Jessica <jessica.bonomelli@beefree.io>
2022-11-18 17:03:40 +0100

    fixes

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -87,15 +87,7 @@ class PublicUserApiTests(TestCase):
         """Test returns error if credentials invalid."""
         create_user(email='test@example.com', password='goodpass')
 
-        payload = {'email': 'test@example.com', 'password': 'badpass'
-        res = self.client.post(TOKEN_URL, payload)
-
-        self.assertNotIn('token', res.data)
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_create_token_email_not_found(self):
-        """Test error returned if user not found for given email."""
-        payload = {'email': 'test@example.com', 'password': 'pass123'}
+        payload = {'email': 'test@example.com', 'password': 'badpass'}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
